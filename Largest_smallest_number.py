f4da2fc97dd1dd713534f89db0aa0fbecd9b2163
KarthikHegde <karthdz@gmail.com>
2016-02-17 16:27:42 -0600

    largest and smallest no for a given input of numbers

---
+++ Largest_smallest_number.py
@@ -0,0 +1,24 @@
+# Find the largest and smallest for a given input of numbers
+
+largest = None
+smallest = None
+
+while True:
+    inp = raw_input('Enter a number: ')
+    if inp == 'done': 
+        break
+
+    try:
+        num = int(inp)
+    except:
+        print 'Invalid input'
+        continue                            
+
+    if smallest is None or num < smallest:
+        smallest = num
+
+    if largest is None or num > largest:
+        largest = num
+
+print 'Maximum is', largest
+print 'Minimum is', smallest
