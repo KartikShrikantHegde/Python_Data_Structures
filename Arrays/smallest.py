0a214b282beed938797aaf3bb6fe4f68caf3eaa9
KarthikHegde <karthdz@gmail.com>
2016-02-24 22:06:53 -0600

    Smallest no in list

--- Arrays/smallest.py
+++ Arrays/smallest.py
@@ -1,11 +1,13 @@
-# Find the smallest no in a list
+# Smallest no in list
 
-def small():
-    my_array = [9, 41, 12, 3, 74, 15,1]
+def smallest(my_array):
+
     smallest = my_array[0]
     for i in my_array:
         if i < smallest:
             smallest= i
     return smallest
 
-print small()
+
+my_array = [9, 41, 12, 3, 74, 15,5]
+print smallest(my_array)
