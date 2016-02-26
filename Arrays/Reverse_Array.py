6ba2a0e556e7d6aad79ef21bdd2ce1804b79c14b
KarthikHegde <karthdz@gmail.com>
2016-02-25 23:37:07 -0600

    Reversing an array with split,strip,and join

---
+++ Arrays/Reverse_Array.py
@@ -0,0 +1,7 @@
+# Reversing an array with split,strip,and join
+
+my_input = int(raw_input())
+if(my_input > 0):
+    my_array = list(raw_input().strip().split(' '))
+    print my_array
+    print ' '.join(my_array[::-1])
