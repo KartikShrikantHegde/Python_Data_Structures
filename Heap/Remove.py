037ccc4410313241530fce83440505a9cbbe0a3b
KarthikHegde <karthdz@gmail.com>
2016-03-03 02:59:21 -0600

    Remove

--- Heap/Remove.py
+++ Heap/Remove.py
@@ -5,15 +5,6 @@ headers = {
     'Accept': 'application/json',
 }
 
-data = {
-    'pay_rate': '10000',
-    'filing_status': 'single',
-    'pay_periods': 1,
-    'state': 'CO',
-    'year':
-    '2014'
-}
-
 r = requests.post(
     'http://taxee.io/api/v1/calculate/2014',
     data=data,
