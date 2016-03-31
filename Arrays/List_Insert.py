9fe2d4e126605bdb000b530263bc4265926b8c13
KarthikHegde <karthdz@gmail.com>
2016-03-31 18:31:13 -0500

    Inserting element into Array

---
+++ Arrays/List_Insert.py
@@ -0,0 +1,10 @@
+#Inserting an element into an array in a specific position
+#Takes O(n) time
+
+my_list = ['Apple','4','Orange']
+
+my_list.insert(2,100)
+print my_list
+my_list.insert(4,"Mango")
+print my_list
+
