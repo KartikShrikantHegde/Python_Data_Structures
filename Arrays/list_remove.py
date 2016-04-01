3d509c51b0f12bd53358b441ac62f138dee821a1
KarthikHegde <karthdz@gmail.com>
2016-03-31 20:05:58 -0500

    Remove, delete, pop operations

--- Arrays/list_remove.py
+++ Arrays/list_remove.py
@@ -6,3 +6,20 @@ my_list = [10,30,50]
 my_list.pop()
 
 print my_list
+
+# Removing an element at the middle
+# Takes O(N) time
+
+new_list = [10,30,50]
+
+del new_list[1]
+
+print new_list
+
+new_list.pop(0)
+
+print new_list
+
+new_list.remove(50)
+
+print new_list
