b9f50a94fd10cf213083fd7a05ab643cea734565
KarthikHegde <karthdz@gmail.com>
2016-04-01 14:04:21 -0500

    Array reversing

--- Arrays/Reverse_Array.py
+++ Arrays/Reverse_Array.py
@@ -1,6 +1,18 @@
 # Reversing an array with split,strip,and join
 
-my_input = int(raw_input())
+
+my_input = int(raw_input().strip())
 if(my_input > 0):
-    my_array = list(raw_input().strip().split(' '))
+    my_array = raw_input().strip().split(" ")
     print ' '.join(my_array[::-1])
+
+'''
+If split is not used it reverses character by character not the whole number. So we need split above.
+
+my_input = int(raw_input().strip())
+if(my_input > 0):
+    my_array = raw_input().strip()
+    print ''.join(my_array[::-1])'''
+
+
+
