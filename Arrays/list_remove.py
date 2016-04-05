7e976e54097112466c2a94a24a5effdac745f872
KarthikHegde <karthdz@gmail.com>
2016-04-04 23:59:28 -0500

    Removing list elements

--- Arrays/list_remove.py
+++ Arrays/list_remove.py
@@ -13,6 +13,7 @@ print my_list
 # Removing an element at the middle
 # Takes O(N) time
 
+# del is faster as it searches based on index value. Hence faster.
 new_list = [10,30,50]
 
 del new_list[1]
@@ -22,6 +23,8 @@ print new_list
 new_list.pop(0)
 
 print new_list
+
+# Remove searches the value in the list and removes value. Hence slower.
 
 new_list.remove(50)
 
