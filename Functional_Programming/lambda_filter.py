0e566bd3ca76a63797247443f17dffdbad34226b
KarthikHegde <karthdz@gmail.com>
2016-04-05 22:29:21 -0500

    Lambda and Filter

--- Functional_Programming/lambda_filter.py
+++ Functional_Programming/lambda_filter.py
@@ -8,5 +8,7 @@ languages = ["HTML", "JavaScript", "Python", "Ruby"]
 print filter(lambda x: x == "Python",languages)
 
 
+
 squares = [x**2 for x in range(1, 11)]
 print filter(lambda x: x >= 30 and x <=70, squares)
+
