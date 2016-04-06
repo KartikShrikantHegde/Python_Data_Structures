f7bbc4bd91e3fe0961c464cda3aa6bc0f8ab4ac8
KarthikHegde <karthdz@gmail.com>
2016-04-05 22:30:55 -0500

    List Comprehension

--- Functional_Programming/list_comprehension.py
+++ Functional_Programming/list_comprehension.py
@@ -7,7 +7,5 @@ new_list = [x for x in range(1,6)]
 evens_to_50 = [i for i in range(51) if i % 2 == 0]
 print evens_to_50
 
-
 # Example 3
-
 even_squares = [x*x for x in range(1,11) if (x) % 2 == 0]
