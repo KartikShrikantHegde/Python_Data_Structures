6726a953acb3dd1123b8f5ee4890d3471cb2735b
KarthikHegde <karthdz@gmail.com>
2016-04-11 20:16:08 -0500

    Create dictionaries

--- Dictionaries/Create_Dict.py
+++ Dictionaries/Create_Dict.py
@@ -7,3 +7,14 @@ d['b'] = 20
 
 for (key,value) in d.items():
     print key,value
+
+
+#####################################
+
+m = {}
+
+m['a'] = 'alpha'
+m['g'] = 'gamma'
+m['o'] = 'omega'
+
+print m
