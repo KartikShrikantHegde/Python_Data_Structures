0d8ae14a8cd17191868ed2b94842db1610fcc776
KarthikHegde <karthdz@gmail.com>
2016-04-16 02:23:39 -0500

    Bit Shift - Only Integer

--- BitWise Operations/Bit_Shift_Operations.py
+++ BitWise Operations/Bit_Shift_Operations.py
@@ -1,6 +1,7 @@
 '''This operation is mathematically equivalent to floor dividing and multiplying by 2 (respectively) for every time you shift, but it's often easier just to think of it as shifting all the 1s and 0s left or right by the specified number of slots.
 
-Note that you can only do bitwise operations on an integer. Trying to do them on strings or floats will result in nonsensical output! '''
+Note that you can only do bitwise operations on an integer.
+Trying to do them on strings or floats will result in nonsensical output! '''
 
 shift_right = 0b1100
 shift_left = 0b1
