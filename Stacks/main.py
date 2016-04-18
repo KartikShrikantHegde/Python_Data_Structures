9ecf7150cc9f30930b290917e688ffaac3128ecd
KarthikHegde <karthdz@gmail.com>
2016-04-18 16:52:42 -0500

    Stack Implementation

---
+++ Stacks/main.py
@@ -0,0 +1,15 @@
+import Stack_Implementation
+
+s=Stack_Implementation.Stack()
+
+print(s.isEmpty())
+s.push(4)
+s.push('dog')
+print(s.peek())
+s.push(True)
+print(s.size())
+print(s.isEmpty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
