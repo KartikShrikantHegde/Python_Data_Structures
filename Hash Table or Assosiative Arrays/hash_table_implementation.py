3d4f713784b39ea88db801b87a11e505cf1c8f98
KarthikHegde <karthdz@gmail.com>
2016-04-18 00:57:35 -0500

    Hash_Table Implementation

---
+++ Hash Table or Assosiative Arrays/hash_table_implementation.py
@@ -0,0 +1,25 @@
+table = [0] * 10
+
+def hash_function(x): return x % 10
+
+def insert(table,input,value):
+    table[hash_function(input)] = value
+    print table
+
+
+insert(table,41,'apple')
+insert(table,93,'banana')
+insert(table,13,'tangerine')
+
+
+# For resolving collision
+
+table = [[] for x in range(10)]
+
+def insert(table,input,value):
+    table[hash_function(input)].append((input,value))
+    print table
+
+insert(table,41,'apple')
+insert(table,93,'banana')
+insert(table,13,'tangerine')
