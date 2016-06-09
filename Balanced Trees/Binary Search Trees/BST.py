1f899e2a39a7960057c614e24cda56d7bbcfef74
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-06-08 23:49:07 -0500

    BST Insert

--- Balanced Trees/Binary Search Trees/BST.py
+++ Balanced Trees/Binary Search Trees/BST.py
@@ -1,3 +1,25 @@
 class BST(object):
-    def __init__(self):
-        self.root = None
+    def __init__(root,data):
+        root.left = None
+        root.right = None
+        root.data = data
+
+def insert(root,node):
+    if root is None:
+        root = node
+    else:
+        if root.data > node.data:
+            if root.left in None:
+                root.left = node
+            else:
+                insert(root.left,node)
+        else:
+            if root.right is None:
+                root.right = node
+            else:
+                insert(root.right,node)
+
+
+
+
+
