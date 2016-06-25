e11380dde743a09e23fc887900d86524c9ac69ba
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-06-25 16:12:21 -0500

    Getters and setters

---
+++ Classes/Setter_Getter.py
@@ -0,0 +1,25 @@
+class Fruit(object):
+
+    def __init__(self, color = "Red"):
+        self._color = color
+
+    def description(self):
+        print "My Discription",self
+
+    def get_color(self):
+        return self._color
+
+    def set_color(self,color):
+        self._color = color
+
+# Creating objects
+
+def main():
+    lemon = Fruit()
+    print lemon.get_color()
+    lemon.set_color('Blue')
+    print lemon.get_color()
+
+
+if __name__ == "__main__":
+    main()
