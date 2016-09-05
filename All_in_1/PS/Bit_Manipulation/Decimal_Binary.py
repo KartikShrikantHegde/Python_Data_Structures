d691d416124f4af2e4de536d136ed7422e248145
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-04 23:05:30 -0500

    Converting Decimal to Binary using stacks

--- All_in_1/PS/Bit_Manipulation/Decimal_Binary.py
+++ All_in_1/PS/Bit_Manipulation/Decimal_Binary.py
@@ -1,17 +1,17 @@
-// Import the already implemented stack
+#  Import the already implemented stack
 
 from Stacks.Stack_Implementation import Stack
 
 def dec_binary(decNumber):
     
-    // Initialize the stack 
+     # Initialize the stack
     myStack = Stack()
 
     while decNumber > 0:
         reminder = decNumber % 2
         myStack.push(reminder)
         
-        // the no is convered to floor value
+        #  the no is convered to floor value
         decNumber = decNumber // 2
 
     binString = ""
