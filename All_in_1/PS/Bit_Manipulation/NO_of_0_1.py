97f6f393ce9f70f13f20e3e269f2786436a2dcae
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-07 21:14:49 -0500

    How many 0's and 1's Binary no using string convert

--- All_in_1/PS/Bit_Manipulation/NO_of_0_1.py
+++ All_in_1/PS/Bit_Manipulation/NO_of_0_1.py
@@ -1,15 +1,22 @@
-def Count_Dig(my_Binary):
-    my_str_binary = str(my_Binary)
-    count_0 = 0
-    count_1 = 0
-    for ch in my_str_binary:
-        if (ch == '1'):
-            count_1 += 1
-        else:
-            count_0 = count_0 + 1
+class Solution(object):
+    def __init__(self, bin_no):
+        self.my_bin = bin_no
 
-    print  "No of zeroe's are: %s" % count_0
-    print  "No of one's are: %s" % count_1
+    def count_dig(self):
+        self.my_bin = str(self.my_bin)
+        count_0 = 0
+        count_1 = 0
+        for ch in self.my_bin:
+            if ch == '1':
+                count_1 += 1
+            else:
+                count_0 += 1
+
+        # return count_0,count_1
+
+        print "No of zero's are: %s" % count_0
+        print "No of one's are: %s" % count_1
 
 
-Count_Dig(1000)
+mySol = Solution(1011)
+mySol.count_dig()
