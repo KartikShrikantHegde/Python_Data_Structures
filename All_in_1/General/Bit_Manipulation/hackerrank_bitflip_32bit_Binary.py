cff3b601a2bb4dc0ba603aadd1f3e1323697dbcd
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-22 02:54:12 -0500

    Calculate no of 1's in binary using bitshift and bitwise operation

--- All_in_1/General/Bit_Manipulation/hackerrank_bitflip_32bit_Binary.py
+++ All_in_1/General/Bit_Manipulation/hackerrank_bitflip_32bit_Binary.py
@@ -1,10 +1,8 @@
 # Convert the binary number to 32 bit and flip the bit and print corresponding binary no
 
 def myfunc(N):
-    #N = N & 0xffffffff # 32 bit representation
-    #print N
     print N ^ 0xffffffff
-    #print bin(N)
+    # or print N ^ 4294967295 (2^32-1)
 
 
 myfunc(2147483647)
