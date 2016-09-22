cca0291319ecb920df24fe484be1fb3b0d1e1349
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2016-09-22 02:12:05 -0500

    Calculate no of 1's in binary using bitshift and bitwise operation

--- All_in_1/LC/Bit_Manipulation/Number of 1 Bits.py
+++ All_in_1/LC/Bit_Manipulation/Number of 1 Bits.py
@@ -1,5 +1,11 @@
 # Calculate the no of 1's in the binary no of a decimal
 
+# # Steps:
+#   1. AND N with 1
+#   2. if the result is one increment count
+#   3. bit-shift n by 1
+#   4. repeat until N is 0
+
 class Solution(object):
     def hammingWeight(self, n):
         """
@@ -7,14 +13,12 @@ class Solution(object):
         :rtype: int
         """
 
-        count = 0
-        my_n = bin(n)
-        for ch in my_n:
-            if ch == '1':
-                count += 1
-
-        return count
-
+        res = 0
+        while n:
+            if n & 1:
+                res += 1
+            n = n >> 1
+        return res
 
 my_sol = Solution()
-print my_sol.hammingWeight(4)
+print my_sol.hammingWeight(11)
