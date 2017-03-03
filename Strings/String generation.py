b0d95b04e2f277407da07e700153467d737a0489
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2017-03-02 18:43:51 -0600

    Backup Commit

--- Strings/String generation.py
+++ Strings/String generation.py
@@ -1,9 +1,12 @@
-print ''.join([`x` for x in xrange(10)])
+# print ''.join([`x` for x in xrange(10)])
 
 #Or
-
+x = [1,2,3]
+print str(x)
 my_list = list()
 for x in range(10):
     my_list.append(`x`)
+print my_list
 
-print ''.join(my_list)
+print '-'.join(my_list)
+print list(my_list)
--- Strings/String_to_List.py
+++ Strings/String_to_List.py
@@ -1,7 +1,40 @@
-# String to list using split
+# String to list using split - split converts to array based on split condition
 
 my_string = "Hi_This is a string to list example using split"
 
 my_list = my_string.split("_")
 
 print my_list
+
+
+# String to list using list keyword - list keyword converts string to list char by char
+
+my_str = "Hey"
+print list(my_str)
+
+# --------------------------------------------------------------------------
+
+# List to string using join - join can be any condition, but join condition will append after 1st char
+
+my_list = ["John","Rick","Tom"]
+print "-".join(my_list)
+
+
+# using string keyword - just converts the list to string
+
+my_list = [1,2,3]
+print str(my_list)
+
+
+# ----------------------------------------------------------------
+
+# str <-> int
+
+
+
+my_str = "123"
+print int(my_str)
+
+my_int = 500
+print str(my_int)
+
