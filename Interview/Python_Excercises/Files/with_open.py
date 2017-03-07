29c52a1b44f95b21e9375e89d412cf3f96ac4e1e
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2017-03-06 23:50:33 -0600

    Update with_open.py

--- Interview/Python_Excercises/Files/with_open.py
+++ Interview/Python_Excercises/Files/with_open.py
@@ -1,3 +1,7 @@
+''' with open is another way of opening the file just like f = open("a.txt","r")'''
+
+''' prints the file as string -> since its a read '''
+
 with open(fileName, 'r') as f:
-  line = f.readline()
+  line = f.read()
   print line
