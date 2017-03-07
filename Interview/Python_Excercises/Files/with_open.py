2ef97aa82a8d8d81d75d7c75025c07e5d3d71011
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2017-03-07 00:54:52 -0600

    Update with_open.py

--- Interview/Python_Excercises/Files/with_open.py
+++ Interview/Python_Excercises/Files/with_open.py
@@ -1,5 +1,7 @@
 ''' with open is another way of opening the file just like f = open("a.txt","r")'''
 
+''' But this is more efficient as it also takes care of file closing '''
+
 ''' prints the file as string -> since its a read '''
 
 with open(fileName, 'r') as f:
