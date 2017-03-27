5c2246ffed6b000f3063dfae95a8f97d28a5cc50
Kartik Shrikant Hegde <kartikshrikanthegde@gmail.com>
2017-03-26 22:30:38 -0500

    Implementation

--- Interview/DSA/Hasing/All_unique_letter_substring.py
+++ Interview/DSA/Hasing/All_unique_letter_substring.py
@@ -1,8 +1,30 @@
-# from collections import Counter
-# def all_unique(st):
-#     for key,value in Counter(st).values():
-#         print key,value
+def all_uniq_sub(s):
 
-print ''.join(set('ababcadbc'))
-# print all_unique(my_str)
-print set('ababcadbc')
+    if not s:
+        return None
+
+    start = 0
+    end = 1
+
+    longest_str = s[0]
+    my_list = []
+    temp_str = longest_str
+    my_list.append(longest_str)
+    while end < len(s):
+        if s[end] not in temp_str:
+            temp_str = temp_str + s[end]
+            end += 1
+        else:
+            if temp_str > longest_str:
+                longest_str = temp_str
+                temp_str = s[end]
+                start = end
+                end += 1
+
+    if len(temp_str) > len(longest_str):
+        return temp_str
+    else:
+        return longest_str
+
+s = 'abacbdadbc'
+print all_uniq_sub(s)
